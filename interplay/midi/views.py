f8363178c9a6a2f697e9871c4b3def6154cfdf4a
keegpwelch <kpwcg2@umsystem.edu>
2021-09-28 13:09:08 -0500

    added some css styling to improve look and feel of site

--- interplay/midi/views.py
+++ interplay/midi/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+def main(req):
+    resp = loader.get_template('midi.html').render({}, req)
+    return HttpResponse(resp)
 
-def index(request):
-    return HttpResponse("Midi Generation Page")
