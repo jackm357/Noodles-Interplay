9165304c503c3450410d49eb140729847a262fbe
david <dphgx8@umsystem.edu>
2021-10-11 18:10:03 -0700

    files are uploadable
    just printing file name, nothing else

--- interplay/interplay/settings.py
+++ interplay/interplay/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-g@t#m+v$b!7i58zd+&orbo1p=s!%hre*s75%du7h=n$^mfxn6i
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'interplay.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,11 +110,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_URL = os.path.join(BASE_DIR, 'interplay/static/')
+# STATIC_URL = os.path.join(BASE_DIR, 'interplay/static/')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -135,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # config/settings.py
 LOGIN_REDIRECT_URL = '/profile'
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- interplay/midi/views.py
+++ interplay/midi/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import loader
+from .forms import UploadFileForm
 
 
 def index(req):
     resp = loader.get_template('midi.html').render({}, req)
     return HttpResponse(resp)
 
-def continue_page(req):
-    resp = loader.get_template('continue.html').render({}, req)
-    return HttpResponse(resp)
+
+def continue_page(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'].name)
+            return HttpResponseRedirect('/midi')
+    else:
+        form = UploadFileForm()
+    return render(request, 'continue.html', {'form': form})
+
+
+def handle_uploaded_file(f):
+    print(f)
