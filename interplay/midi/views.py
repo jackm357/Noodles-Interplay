266d17dc1253cbc1ed2e4b932ed4c6772039826e
keegpwelch <kpwcg2@umsystem.edu>
2021-10-11 23:54:21 -0500

    added interpolate and generate pages, and some styling

--- interplay/midi/views.py
+++ interplay/midi/views.py
@@ -10,6 +10,12 @@ def index(req):
     return HttpResponse(resp)
 
 
+def generate_page(request):
+    if request.method == 'POST':
+        return HttpResponseRedirect('/midi')
+    return render(request, 'generate.html')
+
+
 def continue_page(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
@@ -19,6 +25,17 @@ def continue_page(request):
     else:
         form = UploadFileForm()
     return render(request, 'continue.html', {'form': form})
+
+
+def interpolate_page(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'].name)
+            return HttpResponseRedirect('/midi')
+    else:
+        form = UploadFileForm()
+    return render(request, 'interpolate.html', {'form': form})
 
 
 def handle_uploaded_file(f):
