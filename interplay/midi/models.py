55ea27315db853b9c432d28c66a4afd0534d81da
Ubuntu <ubuntu@ip-172-31-23-202.us-east-2.compute.internal>
2021-11-02 20:22:16 -0500

    Upload backend

--- interplay/midi/models.py
+++ interplay/midi/models.py
@@ -6,3 +6,7 @@ class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class MidiFile(models.Model):
+    midi = models.FileField()
+    midi_data = models.BinaryField(null=True)
