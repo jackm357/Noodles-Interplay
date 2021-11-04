a8da1f53ff59b7b33e82f0f82f6f8617d26b70ea
Jack <jackmarak99@gmail.com>
2021-11-04 13:57:25 -0500

    -Streamlined the file upload process -Implemented the file upload process in the interpolate page -Added some comments to improve readability since this is an important file

--- interplay/midi/views.py
+++ interplay/midi/views.py
@@ -10,36 +10,39 @@ def index(req):
     resp = loader.get_template('midi.html').render({}, req)
     return HttpResponse(resp)
 
-
+# this callback function handles logic on the generate sequence page
 def generate_page(request):
     if request.method == 'POST':
         return HttpResponseRedirect('/midi')
     return render(request, 'generate.html')
 
-
+# This callback function handles logic on the continue sequence page
 def continue_page(request):
     if request.method == 'POST':
         form = UploadMidiForm(request.POST, request.FILES)
         if form.is_valid():
-            uploaded_midi = form.save(commit=False)
-            uploaded_midi.midi_data = form.cleaned_data['midi'].file.read()
-            uploaded_midi.save()
-            return redirect('/')
+            handle_uploaded_file(form)
+            return redirect('/midi')
     else:
         form = UploadMidiForm()
     return render(request, 'continue.html', {'form': form})
 
-
+# This callback function handles logic on the interpolate sequence page
 def interpolate_page(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
+        form = UploadMidiForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'].name)
-            return HttpResponseRedirect('/midi')
+            handle_uploaded_file(form)
+            return redirect('/midi')
     else:
         form = UploadFileForm()
     return render(request, 'interpolate.html', {'form': form})
 
-
-def handle_uploaded_file(f):
-    print(f)
+# Params
+# from - The POST request from the file upload.
+# This function can be used to handle all logic associated with a generic midi file upload
+# Except for the redirect
+def handle_uploaded_file(form):
+    uploaded_midi = form.save(commit=False)
+    uploaded_midi.midi_data = form.cleaned_data['midi'].file.read()
+    uploaded_midi.save()
