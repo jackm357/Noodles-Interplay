103ed35f44a892b3827bd23f811dca02cf883d97
Jack <jackmarak99@gmail.com>
2021-11-04 13:19:36 -0500

    This was not updated in git_target, but was updated in the running code.  We should really just deploy with git.

--- interplay/midi/views.py
+++ interplay/midi/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import loader
-from .forms import UploadFileForm
+from .forms import UploadFileForm, UploadMidiForm
 from django.shortcuts import redirect
 
 
