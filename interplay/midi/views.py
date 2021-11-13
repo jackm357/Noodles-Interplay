be9be64f3d54c1e4865e5efcfa062445f7a85302
Jack <jackmarak99@gmail.com>
2021-11-13 13:48:57 -0600

    Double file upgrade that plays well with David's changes

--- interplay/midi/views.py
+++ interplay/midi/views.py
@@ -60,13 +60,18 @@ def continue_page(request):
 def interpolate_page(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
+        files = request.FILES.getlist('midi')
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'].name)
-            return HttpResponseRedirect('/midi')
+            for f in files:
+                uploaded_midi = UploadMidiForm(midi=f)
+                uploaded_midi.midi_data = form.cleaned_data['midi'].file.read()
+                uploaded_midi.user = request.user.get_username()
+                uploaded_midi.name = uploaded_midi.filename()
+                uploaded_midi.source = "interpolate"
+                uploaded_midi.save()
+
+                subprocess.call(['rm', '-r', 'media/uploaded' + request.user.get_username])
+            return HttpResponseRedirect('/')
     else:
         form = UploadFileForm()
     return render(request, 'interpolate.html', {'form': form})
-
-
-def handle_uploaded_file(f):
-    print(f)
